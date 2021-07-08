00a0d9536b451dfc4a93cf24f58a75bcc7c56a8d
Khwanlada <86813518+Khwanlada@users.noreply.github.com>
2021-07-09 00:26:26 +0700

    4.3

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -18,5 +18,48 @@ app.component('product-display',{
             <button class=" button " :disabled='!inStock':class="{disabledButton: !inStock}" @click="addToCart">Add to Cart</button>
         </div>
     </div>
-</div>`
+</div>`,
+data() {
+    return {
+        product: 'Shoes',
+        brand: 'SE 331',
+        //image: './assets/images/socks_green.jpg',
+        inStock: true,
+        inventory: 100,
+        details: ['50% cotton', '30% wool', '20% polyester'],
+        variants: [
+            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg',quantity:50 },
+            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg',quantity:0 }
+        ],
+        selectedVariant:0,
+        cart: 0
+    }
+},
+methods: {
+    addToCart() {
+        this.cart += 1
+    },
+    updateImage(variantImage) {
+        this.image = variantImage
+    },
+    updateVariant(index){
+        this.selectedVariant = index;
+    }
+    },
+    computed: {
+        title(){
+            return this.brand + ' ' + this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].image;
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].quantity;
+        },
+        displayOnSale(){
+            if(this.inStock == true){
+               return this.brand + ' ' +this.product+ ' '+ 'is on sale'; 
+            }
+        }
+}
 })
