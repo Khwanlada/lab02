ce3140f68fe20f6e8b64074c504c78c34811eadf
Khwanlada <86813518+Khwanlada@users.noreply.github.com>
2021-07-09 00:05:56 +0700

    3.3

--- main.js
+++ main.js
@@ -4,13 +4,14 @@ const app = Vue.createApp({
             product: 'Shoes',
             brand: 'SE 331',
             image: './assets/images/socks_green.jpg',
-            inStock: true,
+            inStock: false,
             inventory: 100,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg',quantity:50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg',quantity:0 }
             ],
+            selectedVariant:0,
             cart: 0
         }
     },
@@ -20,11 +21,20 @@ const app = Vue.createApp({
         },
         updateImage(variantImage) {
             this.image = variantImage
+        },
+        updateVariant(index){
+            this.selectedVariant = index;
         }
         },
         computed: {
             title(){
                 return this.brand + ' ' + this.product
+            },
+            image(){
+                return this.variants[this.selectedVariant].image;
+            },
+            inStock(){
+                return this.variants[this.selectedVariant].quantity;
             }
     }
     
