36074ff1e92b0ee28d68936ff3f799123c450677
Khwanlada <86813518+Khwanlada@users.noreply.github.com>
2021-07-09 00:25:24 +0700

    4.2

---
+++ components/ProductDisplay.js
@@ -0,0 +1,22 @@
+app.component('product-display',{
+    template:
+    /*html*/
+    `<div class="product-display">
+    <div class="product-container">
+        <div class="product-image" :disabled='!inStock':class="{disabledImage: !inStock}">
+            <img :src="image">
+        </div>
+        <div class="product-info">
+            <h1>{{ title }}</h1>
+            <p v-if="inventory > 10">In Stock</p>
+            <p v-else-if="inventory <= 10 && inventory > 0">In Stock</p>
+            <p v-else>Out of Stock</p>
+            <ul>
+                <li v-for="detail in details">{{ detail }}</li>
+            </ul>
+            <div v-for="(variant,index) in variants" :key="variant.id" @mouseover="updateVariant (index)" class="color-circle" :style="{backgroundColor: variant.color}"></div>
+            <button class=" button " :disabled='!inStock':class="{disabledButton: !inStock}" @click="addToCart">Add to Cart</button>
+        </div>
+    </div>
+</div>`
+})
